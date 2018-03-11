478151f31cc93656f643768a94c98b585126154c
Ganesh prasad <gpganeshprasad4@gmail.com>
2018-03-11 23:02:46 +0530

    Logout

--- eclipse-workspace/Login/src/newsreader/LoginForm.java
+++ eclipse-workspace/Login/src/newsreader/LoginForm.java
@@ -14,6 +14,7 @@ import java.io.PrintWriter;
 //import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -38,14 +39,14 @@ import org.w3c.dom.NodeList;
 @WebServlet("/LoginForm")
 public class LoginForm extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginForm() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LoginForm() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -68,129 +69,132 @@ public class LoginForm extends HttpServlet {
 			String userName = request.getParameter("uname");
 			String password = request.getParameter("password");
 			if(!(userName.isEmpty())) {
-			Session session = HibernateUtilities.getSessionFactory().openSession();
-			session.beginTransaction();
-			Users user = session.get(Users.class, userName);
-			session.getTransaction().commit();
-			if(user.getUname() != null) {		
-				String psswrd = user.getPassword(); 
-				if(!(password.equals(psswrd))) {
-					pw.write("<html><body><p>Password is incorrect please try again<a href = \"Index.html\">Login</a></body></html>");
-				}
-				else {
-				String rss = user.getNewspaper_list();
-				if(rss == null) {
-					pw.write("<html><body><p>You haven't registered to any of the News Paper Please Register</p><a href = \"SignUp.html\">Login</a></body></html>");
-				}
-				else {
-				String[] Split = rss.split(",");
-				String l =  new String();
-//				for(String news:Split) {
-//					if(news.equals("Wall street Journul")) {
-//						 doc = dBuilder.parse("http://www.wsj.com/xml/rss/3_7041.xml");
-//					}
-//					else if(news.equals("Deccan Hearld")) {
-//				
-//						
-//						doc = dBuilder.parse("http://www.deccanherald.com/rss-internal/top-stories.rss");
-//					}
-//					else if(news.equals("Kannada Prabha")) {
-//						doc = dBuilder.parse("http://www.kannadaprabha.com/rss/kannada-top-news-1.xml");
-//					}
-//				}
-//					doc.getDocumentElement().normalize();
-//					//System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
-//					NodeList nList = doc.getElementsByTagName("item");
-//					pw.write("<html><head>Top Headlines</head>");
-//
-//					//System.out.println("----------------------------");
-//					for (int temp = 0; temp < nList.getLength(); temp++) {
-//						Node nNode = nList.item(temp);
-//						//if(temp == 0)
-//						//pw.write("<h1>Current Element"+":"+nNode.getNodeName()+"</h1>");
-//						Element eElement = (Element) nNode;
-//						pw.write("<p><b>Title</b>"+":<b>"+ eElement
-//								.getElementsByTagName("title")
-//						.item(0)
-//						.getTextContent()+"</b></p>");
-//						pw.write("<p><b>link</b>"+":"+"<a href=>" +eElement
-//								.getElementsByTagName("link")
-//						.item(0)
-//						.getTextContent()+"</a>"+"</p>");
-////						pw.write("<p>description"+":"+ eElement
-////								.getElementsByTagName("description")
-////						.item(0)
-////						.getTextContent()+"</p>");
-//						pw.write("<p>-------------------------------------------------------------------</p>");
-//					}
-//				}
-//				}
-//			}
-				
-					request.setAttribute("news", Split);
-						String s1 = "<!DOCTYPE html>\r\n" + 
-										"<html>\r\n" + 
-										"<head>\r\n" + 
-										"<meta charset=\"ISO-8859-1\">\r\n" + 
-										"<title>Subscription</title>\r\n" + 
-										"</head>\r\n" + 
-										"<body>\r\n" + 
-										
+				Cookie ck = new Cookie("uname",userName);
+				response.addCookie(ck);
+				System.out.println(ck.getValue());
+				Session session = HibernateUtilities.getSessionFactory().openSession();
+				session.beginTransaction();
+				Users user = session.get(Users.class, userName);
+				session.getTransaction().commit();
+				if(user.getUname() != null) {		
+					String psswrd = user.getPassword(); 
+					if(!(password.equals(psswrd))) {
+						pw.write("<html><body><p>Password is incorrect please try again<a href = \"Index.html\">Login</a></body></html>");
+					}
+					else {
+						String rss = user.getNewspaper_list();
+						if(rss == null) {
+							pw.write("<html><body><p>You haven't registered to any of the News Paper Please Register</p><a href = \"SignUp.html\">Login</a></body></html>");
+						}
+						else {
+							String[] Split = rss.split(",");
+							String l =  new String();
+							//				for(String news:Split) {
+							//					if(news.equals("Wall street Journul")) {
+							//						 doc = dBuilder.parse("http://www.wsj.com/xml/rss/3_7041.xml");
+							//					}
+							//					else if(news.equals("Deccan Hearld")) {
+							//				
+							//						
+							//						doc = dBuilder.parse("http://www.deccanherald.com/rss-internal/top-stories.rss");
+							//					}
+							//					else if(news.equals("Kannada Prabha")) {
+							//						doc = dBuilder.parse("http://www.kannadaprabha.com/rss/kannada-top-news-1.xml");
+							//					}
+							//				}
+							//					doc.getDocumentElement().normalize();
+							//					//System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+							//					NodeList nList = doc.getElementsByTagName("item");
+							//					pw.write("<html><head>Top Headlines</head>");
+							//
+							//					//System.out.println("----------------------------");
+							//					for (int temp = 0; temp < nList.getLength(); temp++) {
+							//						Node nNode = nList.item(temp);
+							//						//if(temp == 0)
+							//						//pw.write("<h1>Current Element"+":"+nNode.getNodeName()+"</h1>");
+							//						Element eElement = (Element) nNode;
+							//						pw.write("<p><b>Title</b>"+":<b>"+ eElement
+							//								.getElementsByTagName("title")
+							//						.item(0)
+							//						.getTextContent()+"</b></p>");
+							//						pw.write("<p><b>link</b>"+":"+"<a href=>" +eElement
+							//								.getElementsByTagName("link")
+							//						.item(0)
+							//						.getTextContent()+"</a>"+"</p>");
+							////						pw.write("<p>description"+":"+ eElement
+							////								.getElementsByTagName("description")
+							////						.item(0)
+							////						.getTextContent()+"</p>");
+							//						pw.write("<p>-------------------------------------------------------------------</p>");
+							//					}
+							//				}
+							//				}
+							//			}
+
+							request.setAttribute("news", Split);
+							String s1 = "<!DOCTYPE html>\r\n" + 
+									"<html>\r\n" + 
+									"<head>\r\n" + 
+									"<meta charset=\"ISO-8859-1\">\r\n" + 
+									"<title>Subscription</title>\r\n" + 
+									"</head>\r\n" + 
+									"<body>\r\n" + 
+
 										"<form action = \"/Login/WebXml\">\r\n" +
-										
-						
+
+
 										"<select name=\"news\">\r\n" ;
-										for(String newsPaper : Split) {
-											l= l + ("<option value=\""+newsPaper+"\">"+newsPaper+"</option>\r\n");
-										}
-										String k = ("</select>\r\n" + 
-										"<input type = \"submit\" value = \"Go\" >\r\n" + 
-										"</form>\r\n" + 
-										"</body>\r\n" + 
-										"</html>");
-				//				request.setAttribute("Split", rss);
-				//				RequestDispatcher rd = request.getRequestDispatcher("display.html");
-				//				rd.forward(request, response);
-								String html = s1 + l + k;
-							    pw.write(html);  
+							for(String newsPaper : Split) {
+								l= l + ("<option value=\""+newsPaper+"\">"+newsPaper+"</option>\r\n");
+							}
+							String k = ("</select>\r\n" + 
+									"<input type = \"submit\" value = \"Go\" >\r\n" + 
+									"</form>\r\n" + 
+									"</body>\r\n" + 
+									"</html>");
+							//				request.setAttribute("Split", rss);
+							//				RequestDispatcher rd = request.getRequestDispatcher("display.html");
+							//				rd.forward(request, response);
+							String html = s1 + l + k;
+							pw.write(html);  
+						}
+					}
 				}
+				//				}
+				//			}
+				//			}
+				else {
+					pw.write("<html><head>Not Register</head><body><p>The User not Registered</p><p>Please register using the below link</p><a href =\"SignUp.html\">SignUp</a></p></body></html>");
 				}
-			}
-//				}
-//			}
-//			}
-			else {
-				pw.write("<html><head>Not Register</head><body><p>The User not Registered</p><p>Please register using the below link</p><a href =\"SignUp.html\">SignUp</a></p></body></html>");
-			}
-			//		}else {
-			//			String[] names = request.getParameterValues("names");
-			//			List list =  Arrays.asList(names);
-			//		}
+				//		}else {
+				//			String[] names = request.getParameterValues("names");
+				//			List list =  Arrays.asList(names);
+				//		}
 
-			// 3. Execute SQL query
-			//				ResultSet rs = stmt.executeQuery();
-			//				rs.next();
-			//				
-			//			}catch(Exception e) {
-			//				pw.write("<html><head>Not Register</head><body><p>The User not Registered</p></body></html>");
-			//			}
-			//			if(rss.isEmpty()) {	
-			//				pw.write("<html><head>Not Register</head><body><p>The User not Registered</p></body></html>");
-			//			}else {
-			//			pw.write("<html><body><p>Logged in succesfully Welcome"+rs.getString(1) +"</p></body></html>");
-			//			}
+				// 3. Execute SQL query
+				//				ResultSet rs = stmt.executeQuery();
+				//				rs.next();
+				//				
+				//			}catch(Exception e) {
+				//				pw.write("<html><head>Not Register</head><body><p>The User not Registered</p></body></html>");
+				//			}
+				//			if(rss.isEmpty()) {	
+				//				pw.write("<html><head>Not Register</head><body><p>The User not Registered</p></body></html>");
+				//			}else {
+				//			pw.write("<html><body><p>Logged in succesfully Welcome"+rs.getString(1) +"</p></body></html>");
+				//			}
 
+			}
+		}catch(Exception e) {
+			System.out.println(e.getLocalizedMessage());
 		}
-	}catch(Exception e) {
-		System.out.println(e.getLocalizedMessage());
 	}
-}
-/**
- * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
- */
-protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	// TODO Auto-generated method stub
-	doGet(request, response);
-}
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
 
 }
--- eclipse-workspace/Login/src/newsreader/WebXml.java
+++ eclipse-workspace/Login/src/newsreader/WebXml.java
@@ -6,6 +6,7 @@ import java.io.PrintWriter;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -24,12 +25,12 @@ import org.w3c.dom.NodeList;
 public class WebXml extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public WebXml() {
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * Default constructor. 
+	 */
+	public WebXml() {
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -38,102 +39,101 @@ public class WebXml extends HttpServlet {
 		// TODO Auto-generated method stub
 		// response.getWriter().append("Served at: ").append(request.getContextPath());
 		response.setContentType("text/html");
+		Document doc = null;
 		PrintWriter pw = response.getWriter();
+		Cookie ck[] = request.getCookies();
+		System.out.println(ck[0].getValue());
 		//RequestDispatcher rd = request.getRequestDispatcher("newspaper.html");
 		try {
-		//File inputFile = new File("./input.xml");
-		//rd.getClass();
-		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-		pw.write("<html><head>XML</head>");
-		String NewsPaper = request.getParameter("news");
-		if(NewsPaper.equals("Wall street Journul")) {
-			Document doc = dBuilder.parse("http://www.wsj.com/xml/rss/3_7041.xml");
+			//File inputFile = new File("./input.xml");
+			//rd.getClass();
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			pw.write("<html><head>XML</head>");
+			String NewsPaper = request.getParameter("news");
+			if(NewsPaper.equals("Wall street Journul")) 
+				doc = dBuilder.parse("http://www.wsj.com/xml/rss/3_7041.xml");
+			else if(NewsPaper.equals("Deccan Hearld")) 
+				doc = dBuilder.parse("http://www.deccanherald.com/rss-internal/top-stories.rss");
+			else if(NewsPaper.equals("Kannada Prabha"))
+				doc = dBuilder.parse("http://www.kannadaprabha.com/rss/kannada-top-news-1.xml");
 			doc.getDocumentElement().normalize();
-			System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
-		    NodeList nList = doc.getElementsByTagName("item");
-		    pw.write("<html><head>Top Headlines:</head>");
-		    
-		    //System.out.println("----------------------------");
-		    for (int temp = 0; temp < nList.getLength(); temp++) {
-		        Node nNode = nList.item(temp);
-		       //if(temp == 0)
-		        	//pw.write("<h1>Current Element"+":"+nNode.getNodeName()+"</h1>");
-		        Element eElement = (Element) nNode;
-		        pw.write("<p>title"+":<b>"+ eElement
-		                   .getElementsByTagName("title")
-		                   .item(0)
-		                   .getTextContent()+"</b></p>");
-		        pw.write("<p>link"+":<b>"+"<href>" +eElement
-		                   .getElementsByTagName("link")
-		                   .item(0)
-		                   .getTextContent()+"</href>"+"</b></p>");
-		        pw.write("<p>description"+":<b>"+ eElement
-		                   .getElementsByTagName("description")
-		                   .item(0)
-		                   .getTextContent()+"</b></p>");
-		        pw.write("<p>-------------------------------------------------------------------</p>");
-		    }
-		}
-		else if(NewsPaper.equals("Deccan Hearld")) {
-			Document doc = dBuilder.parse("http://www.deccanherald.com/rss-internal/top-stories.rss");
-			doc.getDocumentElement().normalize();
-			System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
-		    NodeList nList = doc.getElementsByTagName("item");
-		    pw.write("<html><head>XML</head>");
-		    
-		    //System.out.println("----------------------------");
-		    for (int temp = 0; temp < nList.getLength(); temp++) {
-		        Node nNode = nList.item(temp);
-		       //if(temp == 0)
-		        	//pw.write("<h1>Current Element"+":"+nNode.getNodeName()+"</h1>");
-		        Element eElement = (Element) nNode;
-		        pw.write("<p>title"+":"+ eElement
-		                   .getElementsByTagName("title")
-		                   .item(0)
-		                   .getTextContent()+"</p>");
-		        pw.write("<p>link"+":"+"<href>" +eElement
-		                   .getElementsByTagName("link")
-		                   .item(0)
-		                   .getTextContent()+"</href>"+"</p>");
-		        pw.write("<p>description"+":"+ eElement
-		                   .getElementsByTagName("description")
-		                   .item(0)
-		                   .getTextContent()+"</p>");
-		        pw.write("<p>-------------------------------------------------------------------</p>");
-		    }
-		}
-		else if(NewsPaper.equals("Kannada Prabha")) {
-			Document doc = dBuilder.parse("http://www.kannadaprabha.com/rss/kannada-top-news-1.xml");
-			doc.getDocumentElement().normalize();
-			System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
-		    NodeList nList = doc.getElementsByTagName("item");
-		    pw.write("<html><head>XML</head>");
-		    
-		    //System.out.println("----------------------------");
-		    for (int temp = 0; temp < nList.getLength(); temp++) {
-		        Node nNode = nList.item(temp);
-		       //if(temp == 0)
-		        	//pw.write("<h1>Current Element"+":"+nNode.getNodeName()+"</h1>");
-		        Element eElement = (Element) nNode;
-		        pw.write("<p>title"+":"+ eElement
-		                   .getElementsByTagName("title")
-		                   .item(0)
-		                   .getTextContent()+"</p>");
-		        pw.write("<p>link"+":"+"<href>" +eElement
-		                   .getElementsByTagName("link")
-		                   .item(0)
-		                   .getTextContent()+"</href>"+"</p>");
-		        pw.write("<p>description"+":"+ eElement
-		                   .getElementsByTagName("description")
-		                   .item(0)
-		                   .getTextContent()+"</p>");
-		        pw.write("<p>-------------------------------------------------------------------</p>");
-		    }
-		}
-	}catch(Exception e) {
+			NodeList nList = doc.getElementsByTagName("item");
+			pw.write("<html><head>Top Headlines:</head>");
+			pw.write("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\r\n" + 
+					"<style>\r\n" + 
+					"body {\r\n" + 
+					"    font-family: \"Lato\", sans-serif;\r\n" + 
+					"}\r\n" + 
+					"\r\n" + 
+					".sidenav {\r\n" + 
+					"    width: 130px;\r\n" + 
+					"    position: fixed;\r\n" + 
+					"    z-index: 1;\r\n" + 
+					"    top: 20px;\r\n" + 
+					"    left: 10px;\r\n" + 
+					"    background: #eee;\r\n" + 
+					"    overflow-x: hidden;\r\n" + 
+					"    padding: 8px 0;\r\n" + 
+					"}\r\n" + 
+					"\r\n" + 
+					".sidenav a {\r\n" + 
+					"    padding: 6px 8px 6px 16px;\r\n" + 
+					"    text-decoration: none;\r\n" + 
+					"    font-size: 25px;\r\n" + 
+					"    color: #2196F3;\r\n" + 
+					"    display: block;\r\n" + 
+					"}\r\n" + 
+					"\r\n" + 
+					".sidenav a:hover {\r\n" + 
+					"    color: #064579;\r\n" + 
+					"}\r\n" + 
+					"\r\n" + 
+					".main {\r\n" + 
+					"    margin-left: 140px; /* Same width as the sidebar + left position in px */\r\n" + 
+					"    font-size: 14px; /* Increased text to enable scrolling */\r\n" + 
+					"    padding: 0px 10px;\r\n" + 
+					"}\r\n" + 
+					"\r\n" + 
+					"@media screen and (max-height: 450px) {\r\n" + 
+					"    .sidenav {padding-top: 15px;}\r\n" + 
+					"    .sidenav a {font-size: 18px;}\r\n" + 
+					"}\r\n" + 
+					"</style>\r\n" + 
+					"</head>\r\n" + 
+					"<body>\r\n" + 
+					"\r\n" + 
+					"<div class=\"sidenav\">\r\n" + 
+					"  <a href=\"/Login/LogoutServlet\">Logout</a>\r\n" + 
+					"</div><div class=\"main\">");
+			//System.out.println("----------------------------");
+			for (int temp = 0; temp < nList.getLength(); temp++) {
+				Node nNode = nList.item(temp);
+				//if(temp == 0)
+				//pw.write("<h1>Current Element"+":"+nNode.getNodeName()+"</h1>");
+				Element eElement = (Element) nNode;
+				pw.write("<p><b>Title"+":</b>"+ eElement
+						.getElementsByTagName("description")
+				.item(0)
+				.getTextContent()+"</p>");
+				pw.write("<p><b>Link</b>"+":"+"<a href=\"" +eElement
+						.getElementsByTagName("link")
+				.item(0)
+				.getTextContent()+" \">"+ eElement
+				.getElementsByTagName("title")
+				.item(0)
+				.getTextContent() +"</a>"+"</p>");
+//				pw.write("<p><b>Description"+":</b>"+ eElement
+//						.getElementsByTagName("description")
+//				.item(0)
+//				.getTextContent()+"</p>");
+				pw.write("<p>-------------------------------------------------------------------</p>");
+			}
+
+		}catch(Exception e) {
 			System.out.println(e.getLocalizedMessage());
-	}
+		}
+		
 	}
 
 	/**
