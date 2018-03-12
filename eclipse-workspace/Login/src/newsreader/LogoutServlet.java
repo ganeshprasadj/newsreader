f4bd747d18d58591e7662c069ba26a3860499578
Ganesh prasad <gpganeshprasad4@gmail.com>
2018-03-12 20:12:30 +0530

    Logout

---
+++ eclipse-workspace/Login/src/newsreader/LogoutServlet.java
@@ -0,0 +1,53 @@
+package newsreader;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LogoutServlet
+ */
+@WebServlet("/LogoutServlet")
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LogoutServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String Uname = request.getParameter("uname");
+		Cookie ck = new Cookie("uname",Uname);
+		PrintWriter pw = response.getWriter();
+		ck.setMaxAge(0);
+		ck.getValue();
+		response.addCookie(ck);
+		System.out.println(ck.getValue());
+		pw.write("<html><body><p>You have successfully Logged out</p></body></head>");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
